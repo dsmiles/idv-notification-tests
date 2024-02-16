96415baa7605966ace512eff874c625b9417ff6d
David Smiles <david.smiles@outlook.com>
2024-02-16 19:02:31 +0000

    Tidyup code

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-"# nr-rvnotify-integration-tests" 
+# Introduction
+
+This repo contains a simple REST-assured project for running some notification callback tests against an internal ID&V
+server that I worked on in the past.
+
+The purpose is to make various HTTPS POSTs to a secured resource on a server in the test lab to check the HTTP status
+codes returned from the server.
+
+The test class runs:
+
+- tests with valid notification payloads
+- tests with invalid notification payloads
+- tests with invalid, missing, null parameters
+- tests with valid and invalid authentication tokens
+- tests with missing HTTP payloads
+- verify the HTTP Status codes
+
+### Requirements
+
+This example was written using the following:
+
+- Java 8
+- Maven
+- Git
+- REST-assured [here](https://rest-assured.io)
+- AssertJ [here](https://assertj.github.io/doc/)
+
+### Usage
+
+To run the test class against the authentication endpoint, open a terminal window and enter the commands:
+
+```
+git clone https://github.com/dsmiles/notification-tests.git
+cd notification-tests
+mvn test
+```
+
+### Configuration:
+
+In order to run the tests you must define the following environment variables either from the command line or your IDEs
+run configuration:
+
+| Environment Variable | Description                                       |
+|----------------------|---------------------------------------------------|
+| AUTHENTICATION_TOKEN | Your authentication token from the application UI |
+| VERIFICATION_UUID    | Your verification UUID from the application UI    |
+| APPLICANT_UUID       | Your applicant UUID from the application database |
+
+To obtain the values you must log into your account and access your profile settings on the application UI. The 
+applicant UUID must be retrieved from the database.
+
+I could have defined these variables in a property file, but chose to use environment variables since there were only
+three of them.
---
+++ src/test/java/NotificationTest.java
@@ -0,0 +1,234 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.HttpStatus;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: David Smiles
+ * Date: 11/06/2019
+ * Time: 04:21 PM
+ *
+ * Integration tests for the Notification Callbacks endpoint
+ */
+public class NotificationTest {
+
+    private static RequestSpecification requestSpec;
+
+    private static String authenticationToken;
+
+    private static String verificationUUID;
+
+    private static String applicantUUID;
+
+    private static final String BASE_URI = "https://idvserver.testlab.local";
+
+    private static final String SVC_URL = "/webhook/idv/notificationresult/idvnotify";
+
+    @BeforeClass
+    public static void setup() {
+
+        authenticationToken = System.getenv("AUTHENTICATION_TOKEN");
+        if (authenticationToken == null) {
+            throw new IllegalArgumentException("Authentication token has not been defined");
+        }
+
+        verificationUUID = System.getenv("VERIFICATION_UUID");
+        if (verificationUUID == null) {
+            throw new IllegalArgumentException("Verification UUID has not been defined");
+        }
+
+        applicantUUID = System.getenv("APPLICANT_UUID");
+        if (applicantUUID == null) {
+            throw new IllegalArgumentException("Applicant UUID has not been defined");
+        }
+
+        requestSpec = new RequestSpecBuilder()
+            .setContentType(ContentType.JSON)
+            .setBaseUri(BASE_URI)
+            .setBasePath(SVC_URL)
+            .addFilter(new ResponseLoggingFilter())
+            .addFilter(new RequestLoggingFilter())
+            .build();
+    }
+
+    @Test
+    public void givenValidNotification_whenPosted_thenCreateResource() {
+        // Given
+        Map<String, String> dummyNote = getDummyNote();
+
+        // When
+        Response response = createNotificationResource(authenticationToken, dummyNote);
+
+        // Then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SC_CREATED);
+    }
+
+    @Test
+    public void givenInvalidNotificationData_whenPosted_thenUnprocessableEntity() {
+        // Given
+        Map<String, String> dummyNote = new HashMap<>();
+        dummyNote.put("incorrect_uuid", verificationUUID);
+        dummyNote.put("applicant_uuid", applicantUUID);
+
+        // When
+        Response response = createNotificationResource(authenticationToken, dummyNote);
+
+        // Then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SC_UNPROCESSABLE_ENTITY);
+    }
+
+    @Test
+    public void givenEmptyNotification_whenPosted_thenBadRequest() {
+        // Given
+        Map<String, String> emptyNote = new HashMap<>();
+
+        // When
+        Response response = createNotificationResource(authenticationToken, emptyNote);
+
+        // Then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void givenMissingVerificationId_whenPosted_thenBadRequest() {
+        // Given
+        Map<String, String> invalidNote = new HashMap<>();
+        invalidNote.put("applicant_uuid", applicantUUID);
+
+        // When
+        Response response = createNotificationResource(authenticationToken, invalidNote);
+
+        // Then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void givenMissingApplicantId_whenPosted_thenBadRequest() {
+        // Given
+        Map<String, String> invalidNote = new HashMap<>();
+        invalidNote.put("verification_uuid", applicantUUID);
+
+        // When
+        Response response = createNotificationResource(authenticationToken, invalidNote);
+
+        // Then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SC_CREATED);
+    }
+
+    @Test
+    public void givenNull_whenPosted_thenBadRequest() {
+        // Given
+        Map<String, String> invalidNote = createNote(null, "SomeUUID");
+
+        // When
+        Response response = createNotificationResource(authenticationToken, invalidNote);
+
+        // Then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void givenInvalidAuthenticationToken_whenRequestSecuredResource_thenUnauthorized() {
+        // Given
+        Map<String, String> dummyNote = getDummyNote();
+        String wrongKey = Base64.encodeBase64String("TheWrongToken".getBytes());
+
+        // When
+        Response response = createNotificationResource(wrongKey, dummyNote);
+
+        // Then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SC_UNAUTHORIZED);
+    }
+
+
+    @Test
+    public void givenNoAuthenticationToken_whenRequestSecuredResource_thenUnauthorized() {
+        // Given
+        Map<String, String> dummyNote = getDummyNote();
+
+        // When - Custom POST - no auth token
+        Response response = given()
+            .header("Authorization", "Basic ")
+            .spec(requestSpec)
+            .body(dummyNote)
+            .when()
+            .post()
+            .andReturn();
+
+        // Then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SC_UNAUTHORIZED);
+    }
+
+    @Test
+    public void givenNoAuthorizationHeader_whenRequestSecuredResource_thenUnauthorized() {
+
+        // Given
+        Map<String, String> dummyNote = getDummyNote();
+
+        // When - Custom POST - no authorisation header
+        Response response = given()
+            .spec(requestSpec)
+            .body(dummyNote)
+            .when()
+            .post()
+            .andReturn();
+
+        // Then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SC_UNAUTHORIZED);
+    }
+
+    /**
+     * Creates a new Notification resource on the server.
+     *
+     * @param token  the authentication token
+     * @param params the map of key value pairs representing the notification
+     * @return the HTTP response
+     */
+    private Response createNotificationResource(String token, Map<String, String> params) {
+        String value = "Basic " + Base64.encodeBase64String(token.getBytes());
+
+        return given()
+            .header("Authorization", value)
+            .spec(requestSpec)
+            .body(params)
+            .when()
+            .post()
+            .then()
+            .extract()
+            .response();
+    }
+
+    /**
+     * @return Returns a pre-canned notification
+     */
+    private Map<String, String> getDummyNote() {
+        return createNote(verificationUUID, applicantUUID);
+    }
+
+    /**
+     * Creates a new map of key-value pairs representing the notification
+     *
+     * @param verificationUuid  the UUID for a particular verification
+     * @param applicantUuid     the UUID for a particular applicant
+     * @return a map of key-value pairs
+     */
+    private Map<String, String> createNote(String verificationUuid, String applicantUuid) {
+        Map<String, String> params = new HashMap<>();
+        params.put("verification_uuid", verificationUuid);
+        params.put("applicant_uuid", applicantUuid);
+        return params;
+    }
+}
